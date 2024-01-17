621c97f84e0240c55894c417f6a932685825de0e
musha-tetsuya <the_beatlesan@hotmail.co.jp>
2024-01-17 15:01:17 +0900

    ステート管理機能追加

---
+++ Runtime/StateMangement/StateManager.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MushaLib.StateManagement
+{
+    /// <summary>
+    /// ステート管理
+    /// </summary>
+    public class StateManager
+    {
+        /// <summary>
+        /// 現在のステート
+        /// </summary>
+        public IState CurrentState { get; private set; }
+
+        /// <summary>
+        /// ステートのスタック
+        /// </summary>
+        private Stack<(IState state, Action onPop)> m_StateStack = new();
+
+        /// <summary>
+        /// 次のステートに遷移する
+        /// </summary>
+        public void PushState(IState nextState, Action onPop = null)
+        {
+            this.m_StateStack.Push((this.CurrentState, onPop));
+
+            this.CurrentState = nextState;
+            this.CurrentState?.Start();
+        }
+
+        /// <summary>
+        /// 現在のステートを終了させて、次のステートに遷移する
+        /// </summary>
+        public void ChangeState(IState nextState) 
+        {
+            this.CurrentState?.End();
+            this.CurrentState = nextState;
+            this.CurrentState?.Start();
+        }
+
+        /// <summary>
+        /// 前のステートに戻る
+        /// </summary>
+        public void PopState()
+        {
+            this.CurrentState?.End();
+            this.CurrentState = null;
+
+            if (this.m_StateStack.TryPop(out var item))
+            {
+                this.CurrentState = item.state;
+                item.onPop?.Invoke();
+            }
+        }
+    }
+}
