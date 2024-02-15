dd690d31703a7123cbd6868501bf134c492dae2e
musha-tetsuya <the_beatlesan@hotmail.co.jp>
2024-02-15 18:31:53 +0900

    SelectableListWindow修正

--- DQ/UI/SelectableListWindow.cs
+++ DQ/UI/SelectableListWindow.cs
@@ -1,37 +1,69 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace MushaLib.DQ.UI
 {
     /// <summary>
     /// 選択可能リストウィンドウ
     /// </summary>
-    public abstract class SelectableListWindow : ListWindow
+    public class SelectableListWindow : Window
     {
+        /// <summary>
+        /// GridLayoutGroup
+        /// </summary>
+        [SerializeField]
+        private GridLayoutGroup m_GridLayoutGroup;
+
         /// <summary>
         /// 現在のインデックス
         /// </summary>
         public int CurrentIndex { get; protected set; }
 
         /// <summary>
-        /// Awake
+        /// 選択時イベント
         /// </summary>
-        protected virtual void Awake()
+        public event Action OnSelected;
+
+        /// <summary>
+        /// Start
+        /// </summary>
+        protected virtual void Start()
         {
-            ScrollView.OnUpdateElement += (element, index) =>
+            for (int i = 0; i < m_GridLayoutGroup.transform.childCount; i++)
             {
-                (element as SelectableText).Arrow.SetAnimationType(index == CurrentIndex ? Arrow.AnimationType.Blink : Arrow.AnimationType.Hide);
+                var element = m_GridLayoutGroup.transform.GetChild(i).GetComponent<SelectableText>();
+
+                element.Arrow.SetAnimationType(i == CurrentIndex ? Arrow.AnimationType.Blink : Arrow.AnimationType.Hide);
             };
         }
 
         /// <summary>
-        /// 
+        /// 要素クリック時
+        /// </summary>
+        public void OnClickElement(SelectableText element)
+        {
+            int elementIndex = element.transform.GetSiblingIndex();
+
+            if (CurrentIndex == elementIndex)
+            {
+                Select();
+            }
+            else
+            {
+                SetCurrentIndex(elementIndex);
+            }
+        }
+
+        /// <summary>
+        /// 現在選択中の要素
         /// </summary>
         protected SelectableText GetCurrent()
         {
-            return ScrollView.ScrollElements.Where(x => x.Index == CurrentIndex).OfType<SelectableText>().FirstOrDefault(x => x.gameObject.activeInHierarchy);
+            return m_GridLayoutGroup.transform.GetChild(CurrentIndex).GetComponent<SelectableText>();
         }
 
         /// <summary>
@@ -39,7 +71,7 @@ namespace MushaLib.DQ.UI
         /// </summary>
         public virtual void SetCurrentIndex(int index)
         {
-            index = (int)Mathf.Repeat(index, ScrollView.ElementCount);
+            index = (int)Mathf.Repeat(index, m_GridLayoutGroup.transform.childCount);
 
             if (index != CurrentIndex)
             {
@@ -59,6 +91,8 @@ namespace MushaLib.DQ.UI
             GetCurrent().Arrow.SetAnimationType(Arrow.AnimationType.Show);
 
             CanvasGroup.interactable = false;
+
+            OnSelected?.Invoke();
         }
 
         /// <summary>
