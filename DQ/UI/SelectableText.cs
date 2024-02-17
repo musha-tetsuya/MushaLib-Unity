4e7b08675a3f4e54bd79349c61c233be354c65bd
musha-tetsuya <the_beatlesan@hotmail.co.jp>
2024-02-17 16:08:32 +0900

    DQフォント追加

--- DQ/UI/SelectableText.cs
+++ DQ/UI/SelectableText.cs
@@ -2,7 +2,6 @@ using MushaLib.Localization;
 using MushaLib.UI.InfiniteScrollView;
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.Localization.Components;
 using UnityEngine.UI;
